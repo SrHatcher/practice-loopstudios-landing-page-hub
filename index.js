ede5f5de01321705ef7fc7ce6e359545a43f6861
SrHatcher <erickomaldonado11@gmail.com>
2023-03-10 10:07:07 -0600

    desktop version finished and hover states working, project done

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const navbar = document.querySelector('.header__navbar')
 const headerItems = document.querySelectorAll('.header__item')
 
 function lowerCase(){
-    if(screen.width >= 1000){
+    if(window.innerWidth >= 1000){
+        console.log(window.innerWidth)
         headerItems.forEach((element)=>{
             let lowerText = element.innerText.toLowerCase()
             let correctText = lowerText.charAt(0).toUpperCase() + lowerText.slice(1)
-            
-            element.innerHTML = `<a href="#">${correctText}</a>`
+
+            element.innerHTML = `<a href="#">${correctText}<span class='line'></span></a>`
         })
     }else{
         headerItems.forEach((element)=>{
             let upperCase = element.innerText.toUpperCase()
             
-            element.innerHTML = `<a href="#">${upperCase}</a>`
+            element.innerHTML = `<a href="#">${upperCase}<span class='line'></span></a>`
         })
     }
 }
